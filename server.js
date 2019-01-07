67f10a9bf4d9ef83c4d98cab9f63a0b75514d610
Fredmanzi <manzifred13@gmail.com>
2019-01-07 12:42:18 +0200

    changing port to env

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http =require('http');
 const app = require('./app');
 
-port = process.env.port || 3000;
+port = process.env.port || '3000';
 const server = http.createServer(app);
 server.listen(process.env.port,()=>{
     console.log('server listen on port 3000');
